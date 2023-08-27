a49da7f05bbbd274347a0dce882e9591592bb539
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-08-27 14:46:32 +0000

    Created context for the api base url

--- donation/src/main.jsx
+++ donation/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router} from "react-router-dom";
+import { createContext } from "react";
+
+const ApiBaseUrlContext = createContext('https://cacsaui.tech')
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Router>
-			<App />
+			<ApiBaseUrlContext.Provider>
+				<App />
+			</ApiBaseUrlContext.Provider>
 		</Router>
 	</React.StrictMode>
 );
