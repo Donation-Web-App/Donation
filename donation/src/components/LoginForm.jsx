6db862e1fea3f3f059f6d260dfd4e3970db496e3
Anesu Kafesu <a.kafesu@alustudent.com>
2023-10-27 11:44:46 +0200

    added feedback to login

--- donation/src/components/LoginForm.jsx
+++ donation/src/components/LoginForm.jsx
@@ -3,68 +3,72 @@ import { Form, TextInput, SubmitInput } from ".";
 import { login } from "../lib/api";
 import { storeCredentials } from "../lib/utils";
 import { useState } from "react";
-
+import toast from "react-hot-toast";
 
 export function LoginForm() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [showPassword, setShowPassword] = useState(false);
-	const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
-	async function handleLogin(e) {
-		e.preventDefault();
+  async function handleLogin(e) {
+    toast("Logging you in...");
+    e.preventDefault();
 
-		try {
-			const { token, firstname, role } = await login(email, password);
-			storeCredentials(token, firstname, role);
+    try {
+      const { token, firstname, role } = await login(email, password);
+      storeCredentials(token, firstname, role);
 
-			// Redirect the user based on their role
-			navigate(role == "admin" ? "/admin/home" : "/donor/home");
-		} catch (error) {
-			// Find a better way to handle errors
-			console.log(error);
-		}
-	}
+      // Redirect the user based on their role
+      navigate(role == "admin" ? "/admin/home" : "/donor/home");
+    } catch (error) {
+      toast(
+        "Couldn't log you in. Please check your details and connection and try again."
+      );
+      console.log(error);
+    }
+  }
 
-	return (
-		<Form onSubmit={handleLogin}>
-			<h1 className='text-3xl text-center'>
-				Welcome to <span className='text-primary font-bold'>CACSA-UI Donation</span>
-			</h1>
-			<TextInput
-				type='email'
-				placeholder='Enter your email'
-				onChange={(e) => {
-					setEmail(e.target.value);
-				}}
-				value={email}
-			/>
+  return (
+    <Form onSubmit={handleLogin}>
+      <h1 className="text-3xl text-center">
+        Welcome to{" "}
+        <span className="text-primary font-bold">CACSA-UI Donation</span>
+      </h1>
+      <TextInput
+        type="email"
+        placeholder="Enter your email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        value={email}
+      />
 
-			<TextInput
-				type={showPassword ? "text" : "password"}
-				placeholder='Enter your password'
-				value={password}
-				onChange={(e) => {
-					setPassword(e.target.value);
-				}}
-			/>
+      <TextInput
+        type={showPassword ? "text" : "password"}
+        placeholder="Enter your password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
 
-			<div className='flex items-center justify-between'>
-				<div className='flex items-center gap-2'>
-					<input
-						type='checkbox'
-						onClick={() => setShowPassword((current) => !current)}
-					/>
-					<p> {showPassword ? "Hide" : "Show"} Password </p>
-				</div>
-				<Link to='/forgot'>Forgot Password?</Link>
-			</div>
-			<br />
-			<SubmitInput label='Login' />
-			<p className='text-center mt-5'>
-				Don't have an account?
-				<Link to='/signup'> Sign Up </Link>
-			</p>
-		</Form>
-	);
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            onClick={() => setShowPassword((current) => !current)}
+          />
+          <p> {showPassword ? "Hide" : "Show"} Password </p>
+        </div>
+        <Link to="/forgot">Forgot Password?</Link>
+      </div>
+      <br />
+      <SubmitInput label="Login" />
+      <p className="text-center mt-5">
+        Don't have an account?
+        <Link to="/signup"> Sign Up </Link>
+      </p>
+    </Form>
+  );
 }
